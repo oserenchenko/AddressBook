06d7481d47568fcf57405fa96e6b4e2fef4c7127
Olga <oserenchenko@gmail.com>
2019-01-30 22:05:17 -0700

    add address form created

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import AddressForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = AddressForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('add.html',  title='Sign In', form=form)
