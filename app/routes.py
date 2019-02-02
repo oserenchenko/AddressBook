12562ff1bd4859b3feac5e9e685a7298877297b0
Olga <oserenchenko@gmail.com>
2019-02-02 15:46:06 -0700

    if address is correct add to db table, if incorrect, display that the address is incorrect

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,6 @@ from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import AddressForm
 from app.models import Addresses
-# import requests
-from usps import USPSApi, Address
 from pyusps import address_information
 
 @app.route('/')
@@ -15,35 +13,24 @@ def index():
 def add():
     form = AddressForm()
     if form.validate_on_submit():
-      # address = Address(name=form.name.data, address_1=form.address.data, city=form.city.data, state=form.state.data, zipcode=form.zipcode.data)
-
       addr = dict([
       ('address', form.address.data),
       ('city', form.city.data),
       ('state', form.state.data),
       ('zipcode', form.zipcode.data)
       ])
+
       try:
         address_information.verify('081NA0000050', addr)
         print("good address")
+        address = Addresses(name=form.name.data, address=form.address.data, city=form.city.data, state=form.state.data, zipcode=form.zipcode.data)
+        db.session.add(address)
+        db.session.commit()
+        flash('Address added for {}'.format(form.name.data))
+        return redirect(url_for('index'))
       except Exception as e:
         print(e)
-
-      # if address_information.verify('081NA0000050', addr).startswith('ValueError'):
-      #   print("The address is incorrect")
-
-
-
-
-      # usps = USPSApi('081NA0000050', test=True)
-      # validation = usps.validate_address(address)
-      # print(validation.result)
-
-
-        # db.session.add(address)
-        # db.session.commit()
-        # flash('Address added for {}'.format(form.name.data))
-        # return redirect(url_for('index'))
+        return render_template('add.html',  title='Sign In', form=form, message="Address is incorrect")
     return render_template('add.html',  title='Sign In', form=form)
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete(id):
