61b470358b9e4a0c4090695d738cf9e3401e1bb7
Olga <oserenchenko@gmail.com>
2019-02-02 13:19:42 -0700

    debugged unique records

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from app import db
 
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True, unique=True)
-    address = db.Column(db.String(120), index=True, unique=True)
-    city = db.Column(db.String(120), index=True, unique=True)
-    state = db.Column(db.String(120), index=True, unique=True)
-    zipcode = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(120), index=True)
+    address = db.Column(db.String(120), index=True)
+    city = db.Column(db.String(120), index=True)
+    state = db.Column(db.String(120), index=True)
+    zipcode = db.Column(db.String(120), index=True)
 
     def __repr__(self):
         return '<Address {}>'.format(self.name)    
