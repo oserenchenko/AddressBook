4d617b3f581c3deb37b9985cb710cd6a3f7518cd
Olga <oserenchenko@gmail.com>
2019-02-07 21:21:25 -0700

    deleted migrations

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from app import db
 class Addresses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), index=True)
-    address = db.Column(db.String(120), index=True)
-    city = db.Column(db.String(120), index=True)
-    state = db.Column(db.String(120), index=True)
-    zipcode = db.Column(db.String(120), index=True)
+    address = db.Column(db.String(120))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    zipcode = db.Column(db.String(5))
 
     def __repr__(self):
         return '<Address {}>'.format(self.name)    
